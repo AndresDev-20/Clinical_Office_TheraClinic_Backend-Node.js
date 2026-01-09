db48785d6182977f30684018bccc06b3b823921a
AndresDev20 <andresmarroquin887@gmail.com>
2026-01-09 11:41:34 -0500

    Conetion to DB

---
+++ src/api/config/config.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    database: process.env.DB_NAME_DEV,
+    host: process.env.DB_HOST_DEV,
+    port: process.env.DB_PORT_DEV || 3306,
+    dialect: process.env.DB_DIALECT || 'mysql',
+    logging: true
+  },
+
+  test: {
+    username: process.env.DB_USERNAME_TEST,
+    password: process.env.DB_PASSWORD_TEST,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST_TEST,
+    port: process.env.DB_PORT_TEST || 3306,
+    dialect: process.env.DB_DIALECT || 'mysql',
+    logging: false
+  },
+
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: process.env.DB_DIALECT || 'mysql',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+};
