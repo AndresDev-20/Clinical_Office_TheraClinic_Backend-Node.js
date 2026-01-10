4600936faf21c5272bfde4ac9793d8d70ca9a722
AndresDev20 <andresmarroquin887@gmail.com>
2026-01-10 14:24:30 -0500

    Models

---
+++ src/api/migrations/20260110191156-create-roles.js
@@ -0,0 +1,35 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Roles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nameRole: {
+        type: Sequelize.STRING,
+        unique: true
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      isActive: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Roles');
+  }
+};
