d4cf50a542cb8d1d4b407d6dc69257cd2377fdde
AndresDev20 <andresmarroquin887@gmail.com>
2026-01-18 13:10:33 -0500

    Controllers

--- src/controllers/role.controller.js
+++ src/controllers/role.controller.js
@@ -1,1 +1,13 @@
 const {Roles} = require("../api/models")
+const catchError = require("../utils/catchError")
+
+// Visualizacion de roles
+const getAllRoles = catchError(async(req, res) => {
+    const roles = await Roles.findAll()
+    return res.status(201).json(roles)
+})
+
+
+module.exports = {
+    getAllRoles
+}
