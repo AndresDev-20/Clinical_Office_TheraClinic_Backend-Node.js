d965b232a4cfcc4fac2092c79fd52a295542500e
AndresDev20 <andresmarroquin887@gmail.com>
2026-01-18 13:22:08 -0500

    Controllers

--- src/controllers/role.controller.js
+++ src/controllers/role.controller.js
@@ -7,7 +7,15 @@ const getAllRoles = catchError(async(req, res) => {
     return res.status(201).json(roles)
 })
 
+// Creacion de un rol
+const addRol = catchError(async(req, res) => {
+    const data = req.body;
+    const newRol = await Roles.create(data)
+    return res.status(201).json({message: "El rol se creo exitosamente", newRol})
+})
+
 
 module.exports = {
-    getAllRoles
+    getAllRoles,
+    addRol
 }
