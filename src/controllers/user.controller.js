8a86b4073955c107ee87a6d57cc4541a12646861
AndresDev20 <andresmarroquin887@gmail.com>
2026-01-19 15:05:28 -0500

    Routes | Controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,1 +1,12 @@
 const {User} = require("../api/models")
+const catchError = require("../utils/catchError")
+
+// Viewing users
+const getAllUsers = catchError(async(req, res) => {
+    const users = await User.findAll();
+    return res.status(201).json(users)
+})
+
+module.exports = {
+    getAllUsers
+}
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const roleRouter = require('./role.routes');
+const userRouter = require('./user.routes');
 
 const router = express.Router();
 router.use("/roles", roleRouter)
+router.use("/users", userRouter)
 
 module.exports = router;
