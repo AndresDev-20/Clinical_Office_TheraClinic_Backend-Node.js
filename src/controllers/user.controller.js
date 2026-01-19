57e58ca51c4fcb2641f3acac79e1db0512ffd06f
AndresDev20 <andresmarroquin887@gmail.com>
2026-01-19 15:09:52 -0500

    Controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,6 +7,14 @@ const getAllUsers = catchError(async(req, res) => {
     return res.status(201).json(users)
 })
 
+// Add new user
+const create = catchError(async(req, res) => {
+    const data = req.body;
+    const newUser = await User.create(data)
+    return res.status(201).json({message: "Usuario creado", newUser})
+})
+
 module.exports = {
-    getAllUsers
+    getAllUsers,
+    create
 }
