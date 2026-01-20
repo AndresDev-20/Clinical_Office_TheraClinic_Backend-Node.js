a4d8912cdf9dcbe4f18b8713b2e0506240b60cb4
AndresDev20 <andresmarroquin887@gmail.com>
2026-01-20 09:49:43 -0500

    Middlewares

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
-const router = require("./routers")
+const router = require("./routers");
+const errorHandler = require('./utils/middlewares/errorHandler');
 
 
 // Esta es nuestra app
@@ -16,5 +17,8 @@ app.use(cors());
 
 // Rutas del proyecto
 app.use('/api/v1', router)
+
+// middlewares después de las rutas
+app.use(errorHandler)
 
 module.exports = app;
