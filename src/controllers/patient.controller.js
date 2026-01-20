4387aa6518a56141058de9ba704b2d3498c78273
AndresDev20 <andresmarroquin887@gmail.com>
2026-01-20 12:46:57 -0500

    Controller

---
+++ src/controllers/patient.controller.js
@@ -0,0 +1,8 @@
+const { patient } = require("../api/models")
+const catchError = require("../utils/catchError")
+
+
+// viewing patient
+const getAllPatient = catchError(async(req, res) => {
+    
+})
