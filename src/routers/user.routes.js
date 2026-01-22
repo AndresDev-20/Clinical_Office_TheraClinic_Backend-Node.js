aff45cd07471a9203c8356f600e20ea71b0ad72d
AndresDev20 <andresmarroquin887@gmail.com>
2026-01-22 11:13:32 -0500

    Routes

--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { getAllUsers, create, getUsersById, update } = require('../controllers/user.controller');
+const { getAllUsers, create, getUsersById, update, login } = require('../controllers/user.controller');
 
 const userRouter = express.Router();
 userRouter.route("/")
           .get(getAllUsers)
           .post(create)
+userRouter.route("/login")
+          .post(login)
 userRouter.route("/:id")
           .get(getUsersById)
           .put(update)
