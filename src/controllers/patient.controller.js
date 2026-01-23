501c77fad0df68046cf0f3340a24e7036ab4a3e8
AndresDev20 <andresmarroquin887@gmail.com>
2026-01-23 14:28:42 -0500

    Controllers

--- src/controllers/patient.controller.js
+++ src/controllers/patient.controller.js
@@ -12,7 +12,7 @@ const getAllPatient = catchError(async(req, res) => {
 const getOne = catchError(async(req, res) => {
     const { id } = req.params;
     const patient = await Patient.findByPk(id);
-    if(!patient) return res.status(404).json({message: "El paciente no se encuentra en nuestra base de datos"});
+    if(!patient) return res.status(404).json({Error: "El paciente no se encuentra en nuestra base de datos"});
     return res.status(201).json(patient)
 })
 
@@ -20,7 +20,7 @@ const getOne = catchError(async(req, res) => {
 const create = catchError(async(req, res) => {
     const data = req.body;
     const newPatient = await Patient.create(data);
-    return res.status(201).json({Message: "Usuario creado", newPatient});
+    return res.status(201).json({Message: "Paciente creado", newPatient});
 })
 
 // Update patient by id
@@ -28,10 +28,12 @@ const update = catchError(async(req, res) => {
     const { id } = req.params;
     const data = req.body;
     const updatePatiente = await Patient.update(data, {where: {id}})
+    if(updatePatiente[0] === 0) return res.status(404).json({Error: "El paciente no se encontro en la base de datos"})
 })
 
 module.exports = {
     getAllPatient,
     getOne,
-    create
+    create,
+    update
 }
