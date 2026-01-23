b22a82948e8ef5e5ce1ed07b98109f473f18bb07
AndresDev20 <andresmarroquin887@gmail.com>
2026-01-23 13:47:27 -0500

    Routes

--- src/routers/patient.routes.js
+++ src/routers/patient.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { getAllPatient } = require('../controllers/patient.controller');
+const { getAllPatient, getOne } = require('../controllers/patient.controller');
 
 const patientRouter = express.Router();
 patientRouter.route("/")
              .get(getAllPatient)
+patientRouter.route("/:id")
+             .get(getOne)
 
 module.exports = patientRouter;
