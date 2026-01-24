a96970db574388a2194bb5d17914e29a9585aeb3
AndresDev20 <andresmarroquin887@gmail.com>
2026-01-24 17:15:44 -0500

    Routers

--- src/routers/office.routes.js
+++ src/routers/office.routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const { GetAllOffices } = require('../controllers/office.controller');
+const { getAllOffices, getOneOffice, createOffice, updateOffice, removeOffice } = require('../controllers/office.controller');
 
 const officeRouter = express.Router();
 officeRouter.route("/")
-            .get(GetAllOffices)
+            .get(getAllOffices)
+            .post(createOffice)
+officeRouter.route("/:id")
+            .get(getOneOffice)
+            .put(updateOffice)
+            .delete(removeOffice)
 
 module.exports = officeRouter;
