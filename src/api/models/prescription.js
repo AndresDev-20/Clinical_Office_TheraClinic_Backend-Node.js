d27d714306bea19d7fd4cf0f410d5909ad48c78d
AndresDev20 <andresmarroquin887@gmail.com>
2026-01-28 18:33:25 -0500

    models

--- src/api/models/prescription.js
+++ src/api/models/prescription.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Prescription.belongsTo(models.Product, {
-        
+        foreignKey: "product_id"
       })
     }
   }
