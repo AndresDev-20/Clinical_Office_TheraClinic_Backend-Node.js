b140c9e2c81f7abb974e09675563bb8cd1a82d15
AndresDev20 <andresmarroquin887@gmail.com>
2026-01-27 21:09:20 -0500

    Migrations

---
+++ src/api/migrations/20260128020903-create-prescription.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Prescriptions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      clinical_notes_id: {
+        type: Sequelize.INTEGER
+      },
+      product_id: {
+        type: Sequelize.INTEGER
+      },
+      dose: {
+        type: Sequelize.STRING
+      },
+      frequency: {
+        type: Sequelize.STRING
+      },
+      duration: {
+        type: Sequelize.STRING
+      },
+      observaciones: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Prescriptions');
+  }
+};
---
+++ src/api/models/prescription.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Prescription extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Prescription.init({
+    clinical_notes_id: DataTypes.INTEGER,
+    product_id: DataTypes.INTEGER,
+    dose: DataTypes.STRING,
+    frequency: DataTypes.STRING,
+    duration: DataTypes.STRING,
+    observaciones: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Prescription',
+  });
+  return Prescription;
+};
