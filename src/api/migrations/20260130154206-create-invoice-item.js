e41f25dc98f2d3a34f47779b059f309869f55f6b
AndresDev20 <andresmarroquin887@gmail.com>
2026-01-30 10:46:30 -0500

    MIgrations

--- src/api/migrations/20260130154206-create-invoice-item.js
+++ src/api/migrations/20260130154206-create-invoice-item.js
@@ -10,7 +10,9 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       invoice_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references
       },
       product_id: {
         type: Sequelize.INTEGER
