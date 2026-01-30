9acc93acaaa4bf4885094b206f0cbedfc9cc6ab7
AndresDev20 <andresmarroquin887@gmail.com>
2026-01-29 20:51:35 -0500

    Migrate

--- src/api/migrations/20260130015020-create-invoice.js
+++ src/api/migrations/20260130015020-create-invoice.js
@@ -10,7 +10,8 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       patient_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false
       },
       doctor_id: {
         type: Sequelize.INTEGER
