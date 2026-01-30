5f193f7dc35040346a04b4b379e1855b38eb7219
AndresDev20 <andresmarroquin887@gmail.com>
2026-01-30 10:44:32 -0500

    Models

---
+++ src/api/migrations/20260130154206-create-invoice-item.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('InvoiceItems', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      invoice_id: {
+        type: Sequelize.INTEGER
+      },
+      product_id: {
+        type: Sequelize.INTEGER
+      },
+      amount: {
+        type: Sequelize.STRING
+      },
+      unitPrice: {
+        type: Sequelize.STRING
+      },
+      subTotal: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('InvoiceItems');
+  }
+};
---
+++ src/api/models/invoiceitem.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class InvoiceItem extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  InvoiceItem.init({
+    invoice_id: DataTypes.INTEGER,
+    product_id: DataTypes.INTEGER,
+    amount: DataTypes.STRING,
+    unitPrice: DataTypes.STRING,
+    subTotal: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'InvoiceItem',
+  });
+  return InvoiceItem;
+};
