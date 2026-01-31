701da7e45b038039c742242d53f7840ee50d76f8
AndresDev20 <andresmarroquin887@gmail.com>
2026-01-31 11:53:34 -0500

    fix(models): resolve validation bug in Payment model

--- src/api/models/payment.js
+++ src/api/models/payment.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
     monto: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'Payments',
+    modelName: 'Payment',
   });
   return Payments;
 };
