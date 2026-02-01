b4dc8ba7ba85ac11c410aecfcabf1841038d72bc
AndresDev20 <andresmarroquin887@gmail.com>
2026-02-01 11:41:42 -0500

    feat(controller) add getClinicalRecordsById endpoint

--- src/controllers/clinical-record.controller.js
+++ src/controllers/clinical-record.controller.js
@@ -15,10 +15,20 @@ const createClinicalRecords = catchError(async(req, res) => {
 })
 
 // Filter clinical record by ID
+const getClinicalRecordsById = catchError(async(req, res) => {
+    const { id } = req.params;
+    const clinical = await ClinicalRecord.findByPk(id);
+    if(!clinical) return res.status(404).json({Message: "Historial clinico no entontrado"});
+    return res.status(201).json(clinical);
+})
+
 // Update clinical record by ID
+const 
+
 // Remove clinical record by ID
 
 module.exports = {
     getAllClinicalRecords,
-    createClinicalRecords
+    createClinicalRecords,
+    getClinicalRecordsById
 }
