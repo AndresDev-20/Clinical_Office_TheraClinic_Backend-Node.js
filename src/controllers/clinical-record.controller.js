936ceb1915370c25e55b9aaed9468d0dca5217a7
AndresDev20 <andresmarroquin887@gmail.com>
2026-02-01 19:00:04 -0500

    🚀 feat(daily-comics): start – 2026-02-01

--- src/controllers/clinical-record.controller.js
+++ src/controllers/clinical-record.controller.js
@@ -1,7 +1,17 @@
 const { ClinicalRecord } = require("../api/models")
+const catchError = require("../utils/catchError")
 
 // Viewing clinical records
+const getAllClinicalRecords = catchError(async(req, res) => {
+    const clinicalRecords = await ClinicalRecord.findAll();
+    return res.status(201).json(clinicalRecords);
+})
+
 // Add new clinical record
 // Filter clinical record by ID
 // Update clinical record by ID
 // Remove clinical record by ID
+
+module.exports = {
+    getAllClinicalRecords
+}
